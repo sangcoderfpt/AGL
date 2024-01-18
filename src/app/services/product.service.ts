92327d5754cf8b578e3e9c1372fdf97bb36fa957
sangcdfpt <sangthdph30314@fpt.edu.vn>
2024-01-18 19:01:56 +0700

    push code

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, inject } from '@angular/core'; // inject
+import { HttpClient } from '@angular/common/http'; // HttpClient
+
+import { Observable } from 'rxjs';
+import { Product, ProductAdd, ProductAdmin } from '../components/types/Product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  // call api
+  apiUrl = 'https://fakestoreapi.com/products';
+  apiAdminUrl = 'https://hoadv-nodejs.vercel.app/api/products'; // khai bao apiUrl
+
+  http = inject(HttpClient); // inject bien http
+  constructor() {}
+
+  getProductList(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl); //axios.get(apiUrl)
+  }
+
+  getProductListAdmin(): Observable<ProductAdmin[]> {
+    return this.http.get<ProductAdmin[]>(this.apiAdminUrl); //axios.get(apiUrl)
+  }
+
+  deleteProductById(id: string) {
+    return this.http.delete(`${this.apiAdminUrl}/${id}`);
+  }
+
+  createProduct(product: ProductAdd) {
+    return this.http.post<Product>(this.apiAdminUrl, product);
+  }
+}
